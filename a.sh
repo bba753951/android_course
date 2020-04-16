d04fa31d257592dd3c5415a229c61fc1695ba0f3
bba753951 <bba753951@gmail.com>
2020-04-16 22:02:08 +0800

    first commit

---
+++ a.sh
@@ -0,0 +1,12 @@
+rm -rf ./AAD_1072_HW06-N26071089/.git
+rm -rf ./AAD_1072_HW07-N26071089/.git
+rm -rf ./AAD_1072_HW01-N26071089/.git
+rm -rf ./AAD_1072_HW10-N26071089/.git
+rm -rf ./AAD_1072_HW04-N26071089/.git
+rm -rf ./project/android_StockCalendar/.git
+rm -rf ./AAD_1072_HW09-N26071089/.git
+rm -rf ./AAD_1072_HW02-N26071089/.git
+rm -rf ./AAD_1072_HW05-N26071089/.git
+rm -rf ./AAD_1072_HW08-N26071089/.git
+rm -rf ./android_StockCalendar/.git
+rm -rf ./AAD_1072_HW03-N26071089/.git
